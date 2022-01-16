3d0e6821a8dd33e3b987e259b925f2d4d26f0ab3
Ben <bgrowcott@hotmail.co.uk>
2022-01-16 21:37:06 +0000

    set up local storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,9 +15,9 @@ const hoursArray = [
 ];
 
 //current time and day
-currentDay.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+currentDay.text(moment().format("dddd, MMMM Do YYYY, H:mm:ss"));
 function timeKeeper() {
-  currentDay.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  currentDay.text(moment().format("dddd, MMMM Do YYYY, H:mm:ss"));
 }
 setInterval(timeKeeper, 1000);
 
@@ -38,3 +38,33 @@ function pastPresentFuture() {
 pastPresentFuture();
 // rerun every minute so it updates throughout the day
 setInterval(pastPresentFuture, 60000);
+
+//saving user input to local storage
+let inputArray = $("input");
+let inputValues = [];
+function saveInput() {
+  // buttons.addClass('btn-success')
+  // buttons.removeClass('btn-outline-success')
+  inputValues = [];
+  for (i = 0; i < inputArray.length; i++) {
+    inputValues.push(inputArray[i].value);
+    localStorage.setItem("timeInputs", JSON.stringify(inputValues));
+  }
+}
+
+// putting localStorage into page
+if (localStorage.getItem("timeInputs") == null) {
+  saveInput();
+}
+renderInputs();
+function renderInputs() {
+  for (i = 0; i < inputArray.length; i++) {
+    inputArray[i].value = JSON.parse(localStorage.getItem("timeInputs"))[i];
+  }
+}
+
+//save user inputs on save buttons
+const buttons = $("button");
+for (let button of buttons) {
+  button.addEventListener("click", saveInput);
+}
